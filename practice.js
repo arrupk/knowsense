afefd9044a68db59fd741af1bbfe3b0e00374d99
arrupk <114916182+arrupk@users.noreply.github.com>
2022-10-03 03:57:29 -0700

    task 1

---
+++ practice.js
@@ -0,0 +1,18 @@
+/*<html>
+    <head>
+        <meta>
+        <body>
+<form>
+    <input type= "text" name ="username" />
+    <input type= "email" name ="email" />
+    <input type= "password" name ="pw" />
+
+</form>
+
+        </body>
+        </meta>
+    </head>
+</html>*/
+
+const form = document.getElementById('my-form');
+console.log(form);
